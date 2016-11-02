d8e3582c2a2a372f26d5480e8a2838a1c2eab62a
tfrangi <tristan@frangi.fr>
2016-11-02 16:17:53 +0100

    feature: KYC resource actions: create & get [WIP]

---
+++ Client/KycClient.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Smoney\Smoney\Client;
+
+use Smoney\Smoney\Client\AbstractClient;
+use Smoney\Smoney\Facade\PayoutFacade;
+
+/**
+ * Class KycClient
+ */
+class KycClient extends AbstractClient
+{
+    /**
+     * @param string $appUserId
+     * @param array  $files
+     */
+    public function create($appUserId, $files)
+    {
+        $uri = 'users/'.$appUserId.'/kyc';
+
+        $headers = array(
+            'Authorization' => $this->headers['Authorization'],
+            'Accept' => $this->headers['Accept'],
+        );
+        $multiparts = array();
+        $i = 1;
+        foreach ($files as $file) {
+            $multiparts[] = array(
+                'name'=>'file-'.$i,
+                'contents'=>'file-'.$i,
+                'filename'=>'img/'.$file
+            );
+
+            $i ++;
+        }
+
+        print_r($multiparts);
+        $res = $this->httpClient->request('POST', $this->baseUrl.'/'.$uri, [
+            'headers'=>$headers,
+            'multipart' => $multiparts
+        ])
+        ->getBody()
+        ->getContents();
+
+        return $this->serializer->deserialize($res, 'Smoney\Smoney\Facade\KycFacade', 'json');
+    }
+
+    /**
+     * @param string $appUserId
+     */
+    public function get($appUserId)
+    {
+        $uri = 'users/'.$appUserId.'/kyc';
+        $res = $this->action('GET', $uri);
+
+        return $this->serializer->deserialize($res, 'ArrayCollection<Smoney\Smoney\Facade\KycFacade>', 'json');
+    }
+}
