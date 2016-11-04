f947234168fa7229f0d8925186f913c159854f5d
tfrangi <tristan@frangi.fr>
2016-11-04 16:36:43 +0100

    phpdoc: more details about the array of files uploaded on Kyc creation

--- Client/KycClient.php
+++ Client/KycClient.php
@@ -11,7 +11,7 @@ class KycClient extends AbstractClient
 {
     /**
      * @param string $appUserId
-     * @param array  $files with $value is the whole binary file and $key the name that will be sent as filename
+     * @param array  $files with key as filename and value as file content
      */
     public function create($appUserId, $files)
     {
